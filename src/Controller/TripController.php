665bc2b33e34f29deae61b8c5a0666fbb00fd4c5
Mathias <mathias.robert@contact.com>
2021-03-16 10:46:37 +0000

    add form request for get data

--- src/Controller/TripController.php
+++ src/Controller/TripController.php
@@ -2,7 +2,9 @@
 
 namespace App\Controller;
 
+use App\Entity\Trip;
 use App\Form\TripForm;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -21,10 +23,31 @@ class TripController extends AbstractController
 
     /**
      * @Route("/trip/new", name="newTrip")
+     *
+     * @param Request $request
+     * @return Response
      */
-    public function new ()
+    public function new (Request $request): Response
     {
+
         $form = $this->createForm(TripForm::class);
+        $trip = new Trip();
+
+        $form->handleRequest($request);
+        if ($form->isSubmitted() && $form->isValid()) {
+            // $form->getData() holds the submitted values
+            // but, the original `$task` variable has also been updated
+            $trip = $form->getData();
+            dump($trip);
+            // ... perform some action, such as saving the task to the database
+            // for example, if Task is a Doctrine entity, save it!
+            // $entityManager = $this->getDoctrine()->getManager();
+            // $entityManager->persist($task);
+            // $entityManager->flush();
+
+            return $this->redirectToRoute('task_success');
+        }
+
         return $this->render('/pages/trip/new.html.twig', [
             'form' => $form->createView(),
         ]);
