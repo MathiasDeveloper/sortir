8a778c5947b65e108b0bf9997d8ff36072d3f177
MathiasRbr <mathias.robert35@hotmail.fr>
2021-03-18 14:58:56 +0100

    :sparkles: Add exceptions directory

---
+++ src/Exceptions/Trip/InvalidArgumentException.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Exception;
+
+use Exception;
+
+/**
+ * Class InvalidArgumentException
+ * @package App\Exception
+ */
+class InvalidArgumentException extends Exception
+{
+    /**
+     * You can use this exception for throw error when argument of methods is not valid
+     */
+}
+
+
