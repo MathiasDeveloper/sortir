287c1ed647a0ad1bb1af82f28cf97721d8bffa33
Ewilan Rivière <ewilan.riviere@gmail.com>
2021-03-22 14:48:45 +0100

    🚀 upload photo

--- src/Controller/AccountController.php
+++ src/Controller/AccountController.php
@@ -6,9 +6,11 @@ use App\Form\UserType;
 use App\Entity\Participant;
 use App\Form\UpdatePasswordType;
 use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Component\Filesystem\Filesystem;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\String\Slugger\SluggerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
 
@@ -17,7 +19,7 @@ class AccountController extends AbstractController
     /**
      * @Route("/editer-profil", name="profile_edit")
      */
-    public function edit(Request $request, EntityManagerInterface $entityManager, UserPasswordEncoderInterface $passwordEncoder): Response
+    public function edit(Request $request, EntityManagerInterface $entityManager, UserPasswordEncoderInterface $passwordEncoder, SluggerInterface $slugger): Response
     {
         $user = $this->getUser();
         $form_user = $this->createForm(UserType::class, $user);
@@ -27,6 +29,27 @@ class AccountController extends AbstractController
         $form_user->handleRequest($request);
         if ($form_user->isSubmitted() && $form_user->isValid()) {
             $userData = $form_user->getData();
+
+            // photo management
+            $coverFile = $request->files->get('user')['photoUrl'];
+            if ($coverFile) {
+                $filesystem = new Filesystem();
+                $originalFilename = pathinfo($coverFile->getClientOriginalName(), PATHINFO_FILENAME);
+                $safeFilename = $slugger->slug($originalFilename);
+                $newFilename = $safeFilename.'-'.uniqid().'.'.$coverFile->guessExtension();
+                $coverFile->move(
+                    $this->getParameter('user_photos_directory'),
+                    $newFilename
+                );
+
+                $old_picture = $user->getPhotoUrlRaw();
+                if ($filesystem->exists("uploads/photos/$old_picture")) {
+                    $filesystem->remove("uploads/photos/$old_picture");
+                }
+
+                $user->setPhotoUrl($newFilename);
+            }
+
             $entityManager->persist($userData);
             $entityManager->flush();
 
@@ -40,10 +63,10 @@ class AccountController extends AbstractController
         $form_password->handleRequest($request);
         if ($form_password->isSubmitted() && $form_password->isValid()) {
             $userData = $form_password->getData();
-
             $current_password = $userData['current_password'];
             $user = $this->getUser();
             $checkPass = $passwordEncoder->isPasswordValid($user, $current_password);
+
             if (true === $checkPass) {
                 $new_pwd = $userData['new_password'];
                 $new_pwd_confirm = $userData['new_confirm_password'];
